7ba04767440b8c3f90f308d25ffa079c5bac06fc
Rudy Williams <rudysw05@knights.ucf.edu>
2020-09-12 14:22:58 -0400

    working pipeline from source fs to spacy docs
    untested but working so far
     - index on titles to keep from duplicate inserts to mongo
        (still would like a pickle list of filepaths previously handled)
     - error handling on bulk insert to skip articles duplicated in source fs
     - spacy_doc is array of subdocuments storing spacydoc/model/preproc used

--- preprocessing_pipe.py
+++ preprocessing_pipe.py
@@ -1,6 +1,7 @@
+import datetime
 import os
 import pickle as pkl
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 import spacy
 from spacy.tokens import Doc
 
@@ -27,40 +28,105 @@ def file_generator(data_dir):
                     for t2 in it2:
                         if t2.is_file():
                             with open(t2.path, "r") as f:
-                                title = f.readline()
+                                # remove any potential \n after the title
+                                title = f.readline().strip()
                                 content = f.read()
 
-                            yield (category, title, content)
+                            yield {
+                                "category": category,
+                                "title": title,
+                                "content": content,
+                            }
 
 
-def content_generator(data_dir):
-    files = file_generator(data_dir)
-    for f in files:
-        content = f[-1]
-        yield content
+# the next two generators have to perform two separate reads to be used
+# but there needs to be a way to "lock" the DB until both are done to keep from
+# inconsistent states throwing the _id->content pairs off
 
 
-def spacy_docs_generator(
-    contents, model, preprocessing_func, n_process=1, batch_size=1, disable=None
-):
-    if disable is None:
-        disable = []
+def content_generator(collection):
+    results = collection.find({}, projection={"_id": 0, "content": 1})
+    for r in results:
+        yield r["content"]
+
+
+def id_generator(collection):
+    results = collection.find({}, projection={"_id": 1})
+    for r in results:
+        yield r["_id"]
+
+
+def doc_generator(model, preprocessor, contents, disable, n_process, batch_size):
     nlp = spacy.load(model)
-    nlp.add_pipe(preprocessing_func)
+    nlp.add_pipe(preprocessor)
     return nlp.pipe(
-        contents, batch_size=batch_size, n_process=n_process, disable=disable
+        contents, disable=disable, n_process=n_process, batch_size=batch_size
     )
+
+
+class PreprocessPipeline:
+    def __init__(self, mongodb_collection):
+        self.mongodb_collection = mongodb_collection
+
+    def filesystem_load(self, data_dir):
+        collection = self.mongodb_collection
+        file_data = file_generator(data_dir=data_dir)
+        collection.create_index("title", name="avoid_dups", unique=True)
+        try:
+            result = collection.insert_many(file_data, ordered=False)
+        except errors.BulkWriteError:
+            pass
+        return self
+
+    def spacy_doc_load(self, model, preprocessor, disable, n_process, batch_size):
+        contents = content_generator(collection=self.mongodb_collection)
+        # I think, technically, a lock should be placed on the DB since the DB
+        # reads could happen under diff states (document added btwn reads)
+        # but for my purposes right now there is no need to worry about it
+        ids = id_generator(collection=self.mongodb_collection)
+        docs = doc_generator(
+            model=model,
+            preprocessor=preprocessor,
+            contents=contents,
+            disable=disable,
+            n_process=n_process,
+            batch_size=batch_size,
+        )
+        preprocessor_bytestr = pkl.dumps(preprocessor)
+        date = datetime.datetime.today()
+
+        for _id, doc in zip(ids, docs):
+            doc_bytestr = doc.to_bytes()
+            collection.update_one(
+                {"_id": _id},
+                {
+                    "$push": {
+                        "spacy_doc": {
+                            "date": date,
+                            "model": model,
+                            "preprocessor_bytestr": preprocessor_bytestr,
+                            "doc_bytestr": doc_bytestr,
+                            "doc_sample": doc[:5].text,
+                        }
+                    }
+                },
+            )
+        return
 
 
 if __name__ == "__main__":
-    contents = content_generator("data")
+    from pymongo import MongoClient
 
-    docs = spacy_docs_generator(
-        contents,
-        "en_core_web_sm",
-        preprocess_tokens,
+    client = MongoClient()
+    db = client.bbcDev
+    collection = db.article
+
+    pipeline = PreprocessPipeline(collection)
+    pipeline.filesystem_load("data")
+    pipeline.spacy_doc_load(
+        model="en_core_web_sm",
+        preprocessor=preprocess_tokens,
         disable=["ner", "parser", "tagger"],
+        n_process=4,
         batch_size=50,
-        n_process=1,
     )
-    docs_list = list(docs)
--- trash.py
+++ trash.py
@@ -1,29 +1,3 @@
-def func1():
-    for i in range(10):
-        for j in range(3):
-            if j != 0:
-                yield (i, j)
+import datetime
 
-
-def func2():
-    for obj in func1():
-        yield obj[1]
-
-
-print(len(list(func2())))
-
-
-def func3():
-    for c in [
-        "this is a sentence",
-        "this is another sentence",
-        "I like pizza and pasta.",
-    ] * 100:
-        yield c
-
-
-# import spacy
-
-# nlp = spacy.load("en_core_web_sm")
-# docs = nlp.pipe(func3())
-# print(list(docs))
+print(datetime.datetime.today())

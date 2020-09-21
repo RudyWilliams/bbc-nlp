1c6c8c3a3766c28ec7a15dc1f982d4d8b52e7b65
Rudy Williams <rudysw05@knights.ucf.edu>
2020-09-21 14:57:22 -0400

    adds GET method taking /article?title= arg

---
+++ news_system/api/application.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+from markupsafe import escape
+from pymongo import MongoClient
+from pymongo.collation import Collation
+
+application = Flask(__name__)
+client = MongoClient()
+db = client.bbcDev
+collection = db.article
+
+
+@application.route("/")
+def api_hello():
+    return "Hello from the BBC News Similarity API"
+
+
+@application.route("/article", methods=["GET"])
+def respond_to_article():
+    title = escape(request.args.get("title"))
+    collation = Collation(locale="en", strength=1)
+    result = collection.find_one(
+        {"title": title},
+        projection={"_id": 0, "title": 1, "content": 1},
+        collation=collation,
+    )
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    application.run()

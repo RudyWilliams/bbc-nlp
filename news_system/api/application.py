fa4969dfe5baef1a66fed0adc277d55646c518af
Rudy Williams <rudysw05@knights.ucf.edu>
2020-09-22 16:28:42 -0400

    returns top n_similar articles for title & adds readme

--- news_system/api/application.py
+++ news_system/api/application.py
@@ -5,7 +5,7 @@ from pymongo.collation import Collation
 
 application = Flask(__name__)
 client = MongoClient()
-db = client.bbcDev
+db = client.bbcTest
 collection = db.article
 
 
@@ -16,14 +16,29 @@ def api_hello():
 
 @application.route("/article", methods=["GET"])
 def respond_to_article():
-    title = escape(request.args.get("title"))
+    args = request.args
+    title = escape(args.get("title"))
+    n_similar = int(escape(args.get("nsimilar")))
+
     collation = Collation(locale="en", strength=1)
-    result = collection.find_one(
-        {"title": title},
-        projection={"_id": 0, "title": 1, "category": 1, "content": 1},
+
+    result = collection.aggregate(
+        pipeline=[
+            {"$match": {"title": title}},
+            {
+                "$project": {
+                    "_id": 0,
+                    "spacy_doc": 0,
+                    "similarities.sim_array.other_id": 0,
+                }
+            },  # will need other_id but just checking response
+            {"$unwind": "$similarities.sim_array"},
+            {"$sort": {"similarities.sim_array.sim": -1}},
+            {"$limit": n_similar},
+        ],
         collation=collation,
     )
-    return jsonify(result)
+    return jsonify(list(result))
 
 
 if __name__ == "__main__":
--- trash.py
+++ trash.py
@@ -1,1 +1,27 @@
-import news_system
+from pymongo import MongoClient
+from pymongo.collation import Collation
+
+client = MongoClient()
+db = client.bbcTest
+collection = db.article
+
+# collation = Collation(locale="en", strength=1)
+
+title = "Ad sales boost Time Warner profit"
+n_similar = 2
+
+result = collection.aggregate(
+    pipeline=[
+        {"$match": {"title": title}},
+        {"$project": {"_id": 0, "spacy_doc": 0}},
+        {"$unwind": "$similarities.sim_array"},
+        {"$sort": {"similarities.sim_array.sim": -1}},
+        {"$limit": n_similar},
+    ],
+    # collation=collation, # takes collation arg but no collation index for this collection
+)
+
+for doc in result:
+    print(doc["similarities"]["sim_array"]["sim"])
+
+# print(collection.find_one({"title": title}))
